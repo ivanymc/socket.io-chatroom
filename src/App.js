e4eebffecfd0fdd0dc9226af893bd5155f18e330
Ivan Chan Yin Man <scskchan@gmail.com>
2022-09-29 23:48:43 +0800

    fix cors

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import SendIcon from '@mui/icons-material/Send';
 
 // Connect to backend
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-const socket = io.connect(SERVER_URL);
+const socket = io(SERVER_URL);
 
 // Create MUI Theme
 const darkTheme = createTheme({
